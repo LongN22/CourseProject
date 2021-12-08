72a9d7ae93a02dc5d81d9ac6cbefdeec9c655fb7
thienbinhdinh98 <thienbinhdinh98@gmail.com>
2021-12-08 17:42:53 -0600

    basic js

---
+++ popup.js
@@ -0,0 +1,11 @@
+let link;
+let test = "abc";
+document.getElementById("myButton").addEventListener("click", function() {
+    link = document.getElementById('linkInput').value
+    // console.log(link);
+});
+
+
+
+
+
